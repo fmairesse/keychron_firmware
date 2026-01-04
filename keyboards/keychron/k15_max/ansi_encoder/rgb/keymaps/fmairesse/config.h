4879cf2ded3dff920b2af7badefbf575b0e0f7d6
Fabien Mairesse <fabien.mairesse@alteia.com>
2026-01-04 22:18:47 +0100

    feat(k15max): custom keymap

---
+++ keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/config.h
@@ -0,0 +1,6 @@
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define COMBO_TERM 50 // Time (in ms) that keys must be pressed to register as a combo
+#define COMBO_MUST_HOLD_PER_COMBO // Only trigger combos if all keys are held, not just tapped
