0b686e4bda79ffac814d96c26600263e5052e555
Fabien Mairesse <fabien.mairesse@alteia.com>
2026-01-22 11:08:33 +0100

    feat(k15max): default rgb effect to none

--- keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/config.h
+++ keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/config.h
@@ -4,3 +4,6 @@
 
 #define COMBO_TERM 50 // Time (in ms) that keys must be pressed to register as a combo
 #define COMBO_MUST_HOLD_PER_COMBO // Only trigger combos if all keys are held, not just tapped
+
+// No RGB effects turn on by default
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_NONE
