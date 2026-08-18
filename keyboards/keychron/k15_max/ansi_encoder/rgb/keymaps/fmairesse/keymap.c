967379786d7a024021a7542a1eac5a6b6b634e95
Fabien Mairesse <fabien.mairesse@gevernova.com>
2026-08-18 14:07:01 +0200

    MacOS shortcuts

--- keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/keymap.c
+++ keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/keymap.c
@@ -42,12 +42,14 @@ enum layers {
 #define _ZOOUT   LCTL(KC_MINUS)
 #define _WBAK    LCTL_T(KC_WBAK)
 #define _WFWD    LSFT_T(KC_WFWD)
-// #endregion
+#define _LSCR    LCTL(LGUI(KC_Q)) // Lock screen
 
-
-// Macros
+// Base layers switchers
 #define _TOMAC   DF(MAC_BASE)
 #define _TOWIN   DF(WIN_BASE)
+
+// #endregion Key aliases
+
 
 
 //#region Combos
@@ -99,14 +101,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MC_1,     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGDN,
         MC_2,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,    KC_O,      KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_HOME,
         KC_CAPS,  KC_ESC,   _A,       _S,       _D🍏,     _F,       KC_G,     KC_H,     _J,       _K🍏,    _L,       _SCLN,     KC_QUOT,            KC_ENT,             KC_END,
-        MC_4,     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,    KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
-        MC_5,     KC_LCTL,  KC_LWIN,            _T1LFT,   _T2LFT🍏, _T3LFT🍏,                     _T2RGT,             _T1RGT,   KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LNPAD, KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,    KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        KC_MCTRL, KC_LCTL,  KC_LWIN,            _T1LFT,   _T2LFT🍏, _T3LFT🍏,                     _T2RGT,             _T1RGT,   KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_90(
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY, KC_MNXT,   KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,            _______,
         _TOMAC,   _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _TOWIN,   RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  _______,  RGB_RMOD, KC_PSCR,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_END,
+        _______,  _______,  RGB_RMOD, KC_PSCR,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _LSCR,    _______,  _______,            _______,            KC_END,
         _______,  _______,            _______,  _______,  _______,  _______,  BAT_LVL,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,            _______,  _______,  _______,                      _______,            _______,  _______,            _______,  _______,  _______),
 
