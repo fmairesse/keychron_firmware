8f7788d743b51ddd76c97b670cc3603496613d42
Fabien Mairesse <fabien.mairesse@gevernova.com>
2026-08-19 14:07:57 +0200

    Nav layer for MacOS

--- keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/keymap.c
+++ keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/keymap.c
@@ -8,6 +8,7 @@ enum layers {
     WIN_BASE,
     WIN_FN,
     _NAV,
+    _NAV🍏,
     _NUM
 };
 //#endregion Layers
@@ -16,6 +17,7 @@ enum layers {
 
 // Left alphas
 #define _A       LT(_NAV,KC_A)
+#define _A🍏     LT(_NAV🍏,KC_A)
 #define _S       LALT_T(KC_S)
 #define _D       LCTL_T(KC_D)
 #define _D🍏     LCMD_T(KC_D)
@@ -27,6 +29,7 @@ enum layers {
 #define _K🍏     RCMD_T(KC_K)
 #define _L       LALT_T(KC_L)
 #define _SCLN    LT(_NAV,KC_SCLN)
+#define _SCLN🍏  LT(_NAV🍏,KC_SCLN)
 
 // Thumbs
 #define _T1LFT   LALT_T(KC_BSPC)
@@ -39,9 +42,13 @@ enum layers {
 
 // Shortcuts
 #define _ZOIN    LCTL(KC_EQUAL)
+#define _ZOIN🍏  LCMD(KC_EQUAL)
 #define _ZOOUT   LCTL(KC_MINUS)
+#define _ZOOUT🍏 LCMD(KC_MINUS)
 #define _BAK     LCTL_T(KC_WBAK)
+#define _BAK🍏   LCMD(KC_LBRC)
 #define _FWD     LSFT_T(KC_WFWD)
+#define _FWD🍏   LCMD(KC_RBRC)
 #define _LSCR🍏   LCTL(LGUI(KC_Q)) // Lock screen
 
 // Base layers switchers
@@ -100,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,   KC_F9,     KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
         MC_1,     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGDN,
         MC_2,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,    KC_O,      KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_HOME,
-        KC_CAPS,  KC_ESC,   _A,       _S,       _D🍏,     _F,       KC_G,     KC_H,     _J,       _K🍏,    _L,       _SCLN,     KC_QUOT,            KC_ENT,             KC_END,
+        KC_CAPS,  KC_ESC,   _A🍏,      _S,       _D🍏,     _F,       KC_G,     KC_H,     _J,       _K🍏,    _L,       _SCLN🍏,  KC_QUOT,            KC_ENT,             KC_END,
         KC_LNPAD, KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,    KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
         KC_MCTRL, KC_LCTL,  KC_LWIN,            _T1LFT,   _T2LFT🍏, _T3LFT🍏,                     _T2RGT,             _T1RGT,   KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -133,7 +140,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  KC_ESC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   KC_ESC,   _______,  _______,  _______,            _______,
         _______,  _______,  KC_TAB,   KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_TAB,   _______,            _______,            _______,
-        _______,  _______,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _ZOOUT,   _ZOIN,    _WBAK,    _WFWD,    _______,  _______,  _______,
+        _______,  _______,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _ZOOUT,   _ZOIN,    _BAK,     _FWD,     _______,  _______,  _______,
+        _______,  _______,  _______,            _______,  _______,  _______,                      KC_ENT,             KC_BSPC,  _______,            _______,  _______,  _______),
+
+    [_NAV🍏] = LAYOUT_ansi_90(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  KC_ESC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   KC_ESC,   _______,  _______,  _______,            _______,
+        _______,  _______,  KC_TAB,   KC_LALT,  KC_LCMD,  KC_LSFT,  XXXXXXX,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_TAB,   _______,            _______,            _______,
+        _______,  _______,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _ZOOUT🍏,  _ZOIN🍏,  _BAK🍏,  _FWD🍏,   _______,  _______,  _______,
         _______,  _______,  _______,            _______,  _______,  _______,                      KC_ENT,             KC_BSPC,  _______,            _______,  _______,  _______),
 
     [_NUM] = LAYOUT_ansi_90(
@@ -151,6 +166,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [_NAV]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_NAV🍏]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_NUM]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 
