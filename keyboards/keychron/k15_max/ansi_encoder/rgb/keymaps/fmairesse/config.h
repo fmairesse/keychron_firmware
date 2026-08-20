5d03ffafe1c3abb385b6b7b0100d178fd0d4e063
Fabien Mairesse <fabien.mairesse@gevernova.com>
2026-08-20 13:28:43 +0200

    fixup! Default rgb effect=typing heatmap

--- keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/config.h
+++ keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/config.h
@@ -8,5 +8,6 @@
 #define COMBO_TERM 50 // Time (in ms) that keys must be pressed to register as a combo
 #define COMBO_MUST_HOLD_PER_COMBO // Only trigger combos if all keys are held, not just tapped
 
-// No RGB effects turn on by default
+// Default RGB effect: typing heatmap
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_TYPING_HEATMAP
+
