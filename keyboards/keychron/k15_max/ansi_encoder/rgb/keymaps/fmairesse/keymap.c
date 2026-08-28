1ed2ec145427ac05ba434ecc074a0f7f367a08fa
Fabien Mairesse <fabien.mairesse@alteia.com>
2026-08-28 09:25:39 +0200

    Spotlight shortcut for MacOS

--- keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/keymap.c
+++ keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/fmairesse/keymap.c
@@ -87,6 +87,7 @@ tap_dance_action_t tap_dance_actions[] = {
 #define _FWD🍏   LCMD(KC_RBRC)
 #define _LSCR🍏  LCTL(LGUI(KC_Q)) // Lock screen
 #define _MCTL🍏  TD(_TD_MCTL) // Mission control
+#define _SPTL🍏  LCTL(LGUI(LALT(KC_SPC))) // Spotlight
 
 // Base layers switchers
 #define _TOMAC   DF(MAC_BASE)
@@ -140,7 +141,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_90(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,   KC_F9,     KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
-        MC_1,     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGDN,
+        _SPTL🍏,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGDN,
         MC_2,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,    KC_O,      KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_HOME,
         KC_CAPS,  KC_ESC,   _A🍏,      _S,       _D🍏,     _F,       KC_G,     KC_H,     _J,       _K🍏,    _L,       _SCLN🍏,  KC_QUOT,            KC_ENT,             KC_END,
         KC_LNPAD, KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,    KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
@@ -152,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _TOWIN,   RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  RGB_RMOD, KC_PSCR,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _LSCR🍏,   _______,  _______,            _______,            KC_END,
         _______,  _______,            _______,  _______,  _______,  _______,  BAT_LVL,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,            _______,  _______,  _______,                      _______,            _______,  _______,            _______,  _______,  _______),
+        _______,  _______,  _______,            _______,  _______,  _______,                      _SPTL🍏,            _______,  _______,            _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_ansi_90(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
