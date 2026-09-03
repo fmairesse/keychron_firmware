5e42774f8e78fbf9491a6ffcd8801d1c5010a33b
Fabien Mairesse <fabien.mairesse@alteia.com>
2026-09-03 10:52:23 +0200

    Shortcut for MacOS launchpad

--- keyboards/delast/keymaps/default/keymap.c
+++ keyboards/delast/keymaps/default/keymap.c
@@ -127,6 +127,7 @@ tap_dance_action_t tap_dance_actions[] = {
 #define _FWD🍏   LCMD(KC_RBRC)
 #define _MCTL🍏  TD(_TD_MCTL) // Mission control
 #define _LSCR🍏  LCTL(LGUI(KC_Q)) // Lock screen
+#define _LPAD🍏  LCTL(LGUI(LALT(KC_B)))
 
 //Base layer switch
 #define _TOMAC   DF(_MAC)
@@ -279,7 +280,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FKEYS] = LAYOUT(
         KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,
         _______,   XXXXXXX,   KC_PSCR,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _LSCR🍏,   XXXXXXX,   XXXXXXX,
-        _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_PGUP,   XXXXXXX,
+        _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   _LPAD🍏,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_PGUP,   XXXXXXX,
         _______,   _______,   _______,   KC_MPLY,   _______,   _______,   _______,   _______,   XXXXXXX,   KC_HOME,   KC_PGDN,   KC_END
     ),
 
